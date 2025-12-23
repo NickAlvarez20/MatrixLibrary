2e2c8c4aad3e3fdebb835ed87f08b59257d11bd4
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-12-23 08:09:07 -0700

    fix: fix disconnected git branch

--- main.go
+++ main.go
@@ -1,34 +1,5 @@
 package main
 
-import(
-	"net/http"
-	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
-
-type Book struct {
-	ID     uint   `json:"id" gorm:"primaryKey"`
-	Title  string `json:"title" binding:"required" gorm:"not null"`
-	Author string `json:"author" binding:"required" gorm:"not null"`
-	Year   int    `json:"year" binding:"required,min=1440, max=2026"`
-}
-
-var db *gorm.DB
-
-func initDB(){
-	var err error
-	db, err = gorm.Open(sqlite.Open("books.db"), &gorm.Config{})
-	if err != nil{
-		panic("failed to connect to database")
-	}
-	db.AutoMigrate(&Book{})
-}
-
-
-package main
-
 import (
 	"net/http"
 
